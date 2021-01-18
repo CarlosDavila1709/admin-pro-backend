7b9f4a6bb3d34321dda01b30af8df1eb79683ce1
Carlos Davila <cdavila1709@gmail.com>
2021-01-18 10:14:23 -0500

    Fin Seccion 11

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ dbConnection();
 
 //Rutas
 app.use( '/api/usuarios' , require('./routers/usuarios') );
+app.use( '/api/hospitales' , require('./routers/hospitales') );
+app.use( '/api/medicos' , require('./routers/medicos') );
+app.use( '/api/todo' , require('./routers/busquedas') );
 app.use( '/api/login' , require('./routers/auth') );
+app.use( '/api/uploads' , require('./routers/uploads') );
 
 app.listen( process.env.PORT, () => {
     console.log('servidor corriendo en puerto ' + process.env.PORT);
