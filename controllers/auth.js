796cd50d232636ba8c81eb9c9e6a0b3125987200
Carlos Davila <cdavila1709@gmail.com>
2021-01-18 16:48:33 -0500

    Seccion 12 -  google

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async( req, res = response) => {
 
@@ -50,6 +51,55 @@ const login = async( req, res = response) => {
 
 }
 
+const googleSignIn = async( req, res = response) => {
+
+    const googleToken = req.body.token;
+
+    try{
+
+        const { name, email, picture } = await googleVerify( googleToken );
+
+        const usuarioBD = await Usuario.findOne({ email });
+
+        let usuario  ;
+
+        if( !usuarioBD ){
+            //Si no existe el usuario
+            usuario = new Usuario({ 
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        }else{
+            // Existe usuario
+            usuario = usuarioBD;
+            usuario.google = true;
+        }
+
+        await usuario.save();
+
+        // Generar TOKEN -JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            ok: true,
+            token
+        });
+
+    }catch( error ){
+
+        res.status(401).json({
+            ok: false,
+            msg: "Token no es correcto",
+        
+        });
+    }
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
